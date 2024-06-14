93033635a18a0ded841985ce3b65a83123a75ab2
Emilio Higueras <emhimu@gmail.com>
2024-06-14 20:32:01 +0200

    Initial Commit

---
+++ WorkTracking/WorkTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTrackingApp.swift
+//  WorkTracking
+//
+//  Created by Emilio Higueras Muñoz on 14/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
