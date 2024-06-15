9ed0b765c58ad8d6f45dc9c9cca05ff8b473973b
Emilio Higueras <emhimu@gmail.com>
2024-06-15 14:16:43 +0200

    Se mejora la arquitectura

---
+++ WorkTracking/ButtonDateView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonDateView.swift
+//  WorkTracking
+//
+//  Created by Emilio Higueras Muñoz on 15/6/24.
+//
+
+import SwiftUI
+
+struct ButtonDateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonDateView()
+}
---
+++ WorkTracking/HorasVM.swift
@@ -0,0 +1,8 @@
+//
+//  HorasVM.swift
+//  WorkTracking
+//
+//  Created by Emilio Higueras Muñoz on 15/6/24.
+//
+
+import Foundation
